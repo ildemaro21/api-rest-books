87068ca1a076a0b0d16f551f4c2ec58c31e8816a
ildemaro21 <ildemaro21@gmail.com>
2023-09-08 20:49:05 -0400

    Agregando funcion que regresa una lista de todos los libros y funcion de busqueda por id

--- src/main/java/com/example/obrestdatajpa/controllers/BookController.java
+++ src/main/java/com/example/obrestdatajpa/controllers/BookController.java
@@ -3,7 +3,10 @@ package com.example.obrestdatajpa.controllers;
 import com.example.obrestdatajpa.entities.Book;
 import com.example.obrestdatajpa.repositories.BookRepository;
 import java.util.List;
+import java.util.Optional;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -34,7 +37,17 @@ public class BookController {
     }
     
     //Buscar un libro segun ID
-    
-    
+    @GetMapping("/api/books/{id}")
+    public ResponseEntity<Book> findOneById(@PathVariable Long id){
+        Optional<Book> book = bookRepository.findById(id);
+        if(book.isPresent())
+            return ResponseEntity.ok(book.get());
+        else
+            return ResponseEntity.notFound().build();
+    }
+    /*
+    OTRA FORMA
+    return book.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build()
+    */
 
 }
